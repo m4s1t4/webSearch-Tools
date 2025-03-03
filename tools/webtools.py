e537dcf18b7ada58a3d51e072efb803a5c7cff0b
Your Name <your.email@example.com>
2025-03-03 19:51:36 -0300

    fix: Some minor fixes

--- tools/webtools.py
+++ tools/webtools.py
@@ -1,42 +1,10 @@
-import json
-from typing import List
 from openai import OpenAI
 from dotenv import load_dotenv
 from tavily import TavilyClient
-from dataclasses import dataclass
 from firecrawl import FirecrawlApp
 
 # Upload the api keys
 load_dotenv()
-
-
-@dataclass
-class QueryResponse:
-    response_text: str
-    sources: List[str]
-
-
-# Tool Schema:
-tools = [
-    {
-        "type": "function",
-        "function": {
-            "name": "tavily_search",
-            "description": "Perform a web search using Tavily API to get up-to-date information or additional context. Use this when you need current information or feel a search could provide a better answer.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "query": {"type": "string", "description": "The search query"},
-                    "max_results": {
-                        "type": "integer",
-                        "description": "The maximun number of search results to return. It must be between 0 and 20",
-                    },
-                },
-                "required": ["query", "max_results"],
-            },
-        },
-    },
-]
 
 
 class WebTools:
@@ -44,14 +12,6 @@ class WebTools:
         self.client = OpenAI()
         self.tavily_client = TavilyClient()
         self.firecrawl = FirecrawlApp()
-        self.model = "o3-mini"
-        self.temperature = 0
-        self.reasoning_effort = "high"
-        self.messages = []
-        self.current_conversation = []
-        self.conversations_history = []
-        self.tools = tools
-        self.tool_choice = "required"
 
     def search(self, query: str):
         try:
